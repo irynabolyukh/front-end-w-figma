f4479d8c3d26ffc0bec9521fef4aa999905d59cc
irynabolyukh <iryna.boliukh@ukma.edu.ua>
2020-08-20 11:46:37 +0300

    slides template added

--- js/functions.js
+++ js/functions.js
@@ -1,39 +1,31 @@
+$('.slideshow__slider').slick({
+  arrows: false
+})
+
+$('.left').click(function(){
+  $('.slideshow__slider').slick('slickPrev');
+})
+
+$('.right').click(function(){
+  $('.slideshow__slider').slick('slickNext');
+})
+
+
 //Go to top btn start
-mybutton = document.getElementById("myBtn");
+// mybutton = document.getElementById("myBtn");
 
-window.onscroll = function() {scrollFunction()};
+// window.onscroll = function() {scrollFunction()};
 
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
+// function scrollFunction() {
+//   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//     mybutton.style.display = "block";
+//   } else {
+//     mybutton.style.display = "none";
+//   }
+// }
 
-function topFunction() {
-  document.body.scrollTop = 0; 
-  document.documentElement.scrollTop = 0; 
-}
+// function topFunction() {
+//   document.body.scrollTop = 0; 
+//   document.documentElement.scrollTop = 0; 
+// }
 //Go to top btn end
-
-
-//Show slides start
-var slideIndex = 1;
-showDivs(slideIndex);
-
-function plusDivs(n) {
-  showDivs(slideIndex += n);
-}
-
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  if (n > x.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = x.length}
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
-  }
-  x[slideIndex-1].style.display = "inline-block";  
-}
-//Show slides end
